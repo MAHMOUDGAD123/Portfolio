46f9b56e9b0d8decba972d74684469fd48b8b553
Mahmoud_Gad <hooda.gad333@gmail.com>
2024-12-15 22:43:47 +0200

    roll back again

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    optimizeCss: true,
-    optimisticClientCache: true,
-    workerThreads: true,
-    webpackMemoryOptimizations: true,
-    turbo: {
-      minify: true,
-    },
-    serverSourceMaps: true,
-    serverMinification: true,
-    parallelServerCompiles: true,
-    reactCompiler: true,
-  },
   reactStrictMode: true,
   compiler: {
     removeConsole: true,
