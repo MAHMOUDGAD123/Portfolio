eddfa7f669ec012301ad985535d8e18bf4a9da22
Mahmoud_Gad <hooda.gad333@gmail.com>
2025-04-22 19:38:55 +0200

    fix: GITHUB_TOKEN env on vercel deploy

--- src/utils/server-tools.ts
+++ src/utils/server-tools.ts
@@ -1,34 +1,42 @@
 "use server";
+import { connection } from "next/server";
 
 type KeyValue = {
   [key: string]: unknown;
 };
 
-type RepoType = KeyValue & { name: string };
+type RepoType = KeyValue & { name: string; private: boolean };
 
 // gethub commits data fetching
 export const getGithubCommitCount = async () => {
+  await connection();
+
   if (process.env.NODE_ENV == "development") {
     return (Math.random() * 400 + 100) >>> 0;
   }
 
-  // const githubAccessToken = process.env.GITHUB_TOKEN;
+  const githubAccessToken = process.env.GITHUB_TOKEN;
 
-  // if (!githubAccessToken) {
-  //   throw new Error("GITHUB_TOKEN not found");
-  // }
+  if (!githubAccessToken) {
+    throw new Error("GITHUB_TOKEN not found");
+  }
 
   // try {
-  const res = await fetch(
-    "https://api.github.com/users/MAHMOUDGAD123/repos",
-    {},
-  );
+  const res = await fetch("https://api.github.com/users/MAHMOUDGAD123/repos", {
+    headers: {
+      Authorization: githubAccessToken,
+    },
+  });
   const repos: RepoType[] = await res.json();
   let count = 0;
   for (const repo of repos) {
     const commitsRes = await fetch(
       `https://api.github.com/repos/MAHMOUDGAD123/${repo.name}/commits`,
-      {},
+      {
+        headers: {
+          Authorization: githubAccessToken,
+        },
+      },
     );
     const commits: KeyValue[] = await commitsRes.json();
     count += commits.length || 0;
