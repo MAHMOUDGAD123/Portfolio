e37f045251d404be4e531260dd2d39267c66f5f9
Mahmoud_Gad <hooda.gad333@gmail.com>
2025-04-22 13:32:05 +0200

    fix: Github commit count fails

--- src/utils/server-tools.ts
+++ src/utils/server-tools.ts
@@ -12,7 +12,7 @@ export const getGithubCommitCount = async () => {
     return (Math.random() * 400 + 100) >>> 0;
   }
 
-  const githubAccessToken = process.env.GITHUB_TOKEN;
+  const githubAccessToken = "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";
 
   if (!githubAccessToken) {
     throw new Error("GITHUB_TOKEN not found");
