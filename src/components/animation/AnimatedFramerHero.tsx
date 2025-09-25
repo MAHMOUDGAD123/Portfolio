5a3dbb3e0d6c47d45fe795c6b2d0c6bc73ee06f5
Mahmoud_Gad <hooda.gad333@gmail.com>
2025-09-25 16:30:22 +0300

    update: change hero image

--- src/components/animation/AnimatedFramerHero.tsx
+++ src/components/animation/AnimatedFramerHero.tsx
@@ -1,10 +1,11 @@
 import "@/styles/animated-framer-hero.css";
+import Image from "next/image";
 
 export default function AnimatedFramerHero() {
   return (
-    <div className="animated-framer-text" >
+    <div className="animated-framer-text">
       <div className="img-box">
-        <img src='imgs/ME.png' alt="ME" />
+        <Image src="/imgs/ME.webp" alt="ME" width={500} height={500} priority />
       </div>
     </div>
   );
